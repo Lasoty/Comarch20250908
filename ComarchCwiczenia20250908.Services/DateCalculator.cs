0ef6957958e2a27efff1544debab2a546732dbd8
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-09-08 13:58:28 +0200

    Initial Commit

---
+++ ComarchCwiczenia20250908.Services/DateCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace ComarchCwiczenia20250908.Services;
+
+public class DateCalculator
+{
+    public DateTime GetNextBusinessDay(DateTime date)
+    {
+        do
+        {
+            date = date.AddDays(1);
+        } while (date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday);
+
+        return date;
+    }
+}
---
+++ ComarchCwiczenia20250908.UnitTests/DateCalculatorTests.cs
@@ -0,0 +1,54 @@
+﻿using ComarchCwiczenia20250908.Services;
+
+namespace ComarchCwiczenia20250908.UnitTests;
+
+[TestFixture]
+public class DateCalculatorTests
+{
+    private DateCalculator cut = null!;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        Console.WriteLine("OneTimeSetUp");
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        cut = new DateCalculator();
+    }
+
+    [Test]
+    public void GetNextBusinessDay_Should_SkipWeekends()
+    {
+        // Arrange
+        DateTime friday = new DateTime(2025, 9, 5); 
+        DateTime saturday = new DateTime(2025, 9, 6); 
+        DateTime sunday = new DateTime(2025, 9, 7); 
+        DateTime expected = new DateTime(2025, 9, 8);
+
+        // Act
+        DateTime resultForFriday = cut.GetNextBusinessDay(friday);
+        DateTime resultForSaturday = cut.GetNextBusinessDay(saturday);
+        DateTime resultForSunday = cut.GetNextBusinessDay(sunday);
+
+        // Assert
+        Assert.That(resultForFriday, Is.EqualTo(expected));
+        Assert.That(resultForSaturday, Is.EqualTo(expected));
+        Assert.That(resultForSunday, Is.EqualTo(expected));
+
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Console.WriteLine("TearDown");
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        Console.WriteLine("OneTimeTearDown");
+    }
+}
