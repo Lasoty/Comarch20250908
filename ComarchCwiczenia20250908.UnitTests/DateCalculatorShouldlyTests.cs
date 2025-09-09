fc5bf792476725f817948d2c800eba082bc6b688
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-09-09 09:40:05 +0200

    Testowanie dat za pomocą Shouldly

---
+++ ComarchCwiczenia20250908.UnitTests/DateCalculatorShouldlyTests.cs
@@ -0,0 +1,32 @@
+﻿using ComarchCwiczenia20250908.Services;
+using Shouldly;
+
+namespace ComarchCwiczenia20250908.UnitTests;
+
+[TestFixture]
+public class DateCalculatorShouldlyTests
+{
+    private DateCalculator cut;
+
+    [SetUp]
+    public void Setup()
+    {
+        cut = new DateCalculator();
+    }
+
+    [Test]
+    public void GetNextBusinessDay_Should_SkipWeekends()
+    {
+        cut.GetNextBusinessDay(new DateTime(2025,9,5)).ShouldBe(new DateTime(2025,9,8));
+        cut.GetNextBusinessDay(new DateTime(2025,9,6)).ShouldBe(new DateTime(2025,9,8));
+        cut.GetNextBusinessDay(new DateTime(2025,9,7)).ShouldBe(new DateTime(2025,9,8));
+    }
+
+    [Test]
+    public void GetNextBusinessDay_Should_BeAfterInputDate()
+    {
+        var date = new DateTime(2025, 9, 8);
+        cut.GetNextBusinessDay(date).ShouldBeGreaterThan(date);
+    }
+
+}
