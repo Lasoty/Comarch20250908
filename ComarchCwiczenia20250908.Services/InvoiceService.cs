8a59b56171c635bb39bdce81d3481ccd3282efb3
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-09-09 10:12:10 +0200

    Testy shouldly dla string.

---
+++ ComarchCwiczenia20250908.Services/InvoiceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComarchCwiczenia20250908.Services;
+public class InvoiceService
+{
+    public string GenerateInvoiceNumber()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ComarchCwiczenia20250908.UnitTests/InvoiceServiceShouldlyTests.cs
@@ -0,0 +1,50 @@
+﻿using ComarchCwiczenia20250908.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace ComarchCwiczenia20250908.UnitTests;
+
+[TestFixture]
+public class InvoiceServiceShouldlyTests
+{
+    private InvoiceService cut;
+
+    [SetUp]
+    public void Setup()
+    {
+        cut = new InvoiceService();
+    }
+
+    [Test]
+    public void GenerateInvoiceNumber_ShouldStartWith_INV()
+    {
+        cut.GenerateInvoiceNumber().ShouldStartWith("INV-");
+        
+        //var acual = cut.GenerateInvoiceNumber();
+        //acual.ShouldStartWith("INV-");
+    }
+
+    [Test]
+    public void GenerateInvoiceNumber_ShouldEndWithNumeriSuffix()
+    {
+        cut.GenerateInvoiceNumber().ShouldMatch(@"^INV-\d{8}-\d{3}$");
+    }
+
+    [Test]
+    public void GenerateInvoiceNumber_ShouldContain_CurrentDate()
+    {
+        var currentDate = DateTime.Now.ToString("yyyyMMdd");
+        cut.GenerateInvoiceNumber().ShouldContain(currentDate);
+    }
+
+    [Test]
+    public void GenerateInvoiceNumber_ShouldHaveLength_Of_16()
+    {
+        cut.GenerateInvoiceNumber().Length.ShouldBe(16);
+    }
+
+}
